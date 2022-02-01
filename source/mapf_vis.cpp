406bd9e5d79fd62708ce4ec46e149b55c74353a8
MShepelin <shepelin.d@ya.ru>
2022-02-01 16:00:46 +0300

    fix: main functions separation

--- source/mapf_vis.cpp
+++ source/mapf_vis.cpp
@@ -48,118 +48,181 @@ public:
   {}
 };
 
+class Mission
+{
+  ScenarioLoader loader;
+  std::shared_ptr<SpaceTime> space;
+  std::ofstream animation;
+  Time depth = 0;
+  int agentsNum = 0;
+
+public:
+  Mission(const char* scenariosFileName)
+    : loader(scenariosFileName)
+  {
+    assert(loader.GetNumExperiments() > 0);
+  }
+
+  int InitAnimation(const char* animationFileName)
+  {
+    std::cout << animationFileName << "\n";
+    animation.open(animationFileName, std::ios_base::out);
+    if (!animation.is_open()) return 1;
+    return 0;
+  }
+
+  int ReadSpace(Time inDepth, const char* spaceFileName)
+  {
+    if (!animation.is_open()) return 1;
+
+    SpaceReader reader;
+    std::ifstream spaceFile(spaceFileName);
+    if (!spaceFile.is_open()) return 1;
+
+    std::optional<RawSpace> rawSpace = reader.FromHogFormat(spaceFile);
+    if (!rawSpace.has_value()) return 1;
+
+    depth = inDepth;
+    space = std::make_shared<SpaceTime>(inDepth, rawSpace.value());
+
+    animation << rawSpace.value().GetWidth() << "\n";
+    for (int i = 0; i < (int)rawSpace.value().GetHeight(); ++i)
+    {
+      for (int j = 0; j < (int)rawSpace.value().GetWidth(); ++j)
+      {
+        if (rawSpace.value().GetAccess({ i, j }) != Access::Accessable)
+        {
+          animation << "@";
+        }
+        else
+        {
+          animation << ".";
+        }
+      }
+
+      animation << "\n";
+    }
+
+    return 0;
+  }
+
+  int InitAgents(int numAgents)
+  {
+    agentsNum = numAgents;
+    if (agentsNum > loader.GetNumExperiments())
+    {
+      return 1;
+    }
+
+    for (int i = 0; i < agentsNum; ++i)
+    {
+      Experiment agent = loader.GetNthExperiment(i);
+      space->SetAccess({ {agent.GetStartX(), agent.GetStartY()}, {0, depth} }, Access::Inaccessable);
+    }
+
+    return 0;
+  }
+
+  int SolveCycle()
+  {
+    for (int i = 0; i < agentsNum; ++i)
+    {
+      // TODO add test when agent stands on one place
+
+      std::cout << "Planning agent " << i << "... ";
+
+      // Prepare agent
+      Experiment agent = loader.GetNthExperiment(i);
+      Point start = { agent.GetStartX(), agent.GetStartY() };
+      Point goal = { agent.GetGoalX(), agent.GetGoalY() };
+
+      const SegmentHolder& segHolder = space->GetSegments(goal);
+      const auto iter = --segHolder.end(); // todo assert if any exists
+      Area origin = { start, {0, depth} };
+      Area destination = { goal, *iter };
+
+      space->SetAccess(origin, Access::Accessable);
+
+      // Prepare pathfinding
+      ArrayType<Move<Point>> moves =
+      {
+        Move<Point>{ 1, {0, 1}},
+        Move<Point>{ 1, {0, -1}},
+        Move<Point>{ 1, {1, 0}},
+        Move<Point>{ 1, {-1, 0}},
+      };
+
+      std::shared_ptr<MovesTestSegment> movesComponent(new MovesTestSegment(moves, space.get()));
+
+      std::shared_ptr<EuclideanHeuristic> hpoint(new EuclideanHeuristic(destination.point));
+      std::shared_ptr<Heuristic<Area>> h(new SpaceAdapter<Point, Area>(hpoint));
+
+      Pathfinder<Area> simplePathfinding(movesComponent, origin, h);
+
+      // Execute pathfinding
+      simplePathfinding.FindCost(destination);
+      if (!simplePathfinding.IsCostFound(destination))
+      {
+        std::cout << "failed to find agent with id = " << i << "\n";
+        return 1;
+      }
+
+      ArrayType<Node<Area>> path;
+      simplePathfinding.CollectPath(destination, path);
+      ArrayType<Area> inaccessableParts;
+      FromPathToFilledAreas(path, inaccessableParts);
+      space->MakeAreasInaccessable(inaccessableParts);
+
+      animation << "Agent " << i;
+      animation << " " << inaccessableParts[0].point.x << " " << inaccessableParts[0].point.y << " " << std::setprecision(4) << inaccessableParts[0].interval.start;
+      for (size_t i = 0; i + 1 < inaccessableParts.size(); i += 2)
+      {
+        Area currentArea = inaccessableParts[i];
+        Area nextArea = inaccessableParts[i + 1];
+        assert(nextArea.interval.IsValid());
+        animation << " " << currentArea.point.x << " " << currentArea.point.y << " " << std::setprecision(4) << nextArea.interval.start;
+        animation << " " << nextArea.point.x << " " << nextArea.point.y << " " << std::setprecision(4) << nextArea.interval.end;
+      }
+      animation << " " << inaccessableParts.back().point.x << " " << inaccessableParts.back().point.y << " " << std::setprecision(4) << inaccessableParts.back().interval.end;
+      animation << "\n";
+
+      std::cout << "success!\n";
+    }
+
+    return 0;
+  }
+};
+
 int main()
 {
-  std::ofstream animation(TEST_DATA_PATH "/animation.txt");
-  if (!animation.is_open()) return 1;
+  Mission mission(TEST_DATA_PATH "/empty-16-16-even-1.scen");
 
-  // Read map
-  SpaceReader reader;
-  std::ifstream spaceFile(TEST_DATA_PATH "/empty-16-16.map");
-  if (!spaceFile.is_open()) return 1;
-
-  std::optional<RawSpace> rawSpace = reader.FromHogFormat(spaceFile);
-  if (!rawSpace.has_value()) return 1;
-  std::cout << "Map is ready\n";
-
-  animation << rawSpace.value().GetWidth() << "\n";
-  for (int i = 0; i < (int) rawSpace.value().GetHeight(); ++i)
+  if (mission.InitAnimation(TEST_DATA_PATH "/animation.txt"))
   {
-    for (int j = 0; j < (int) rawSpace.value().GetWidth(); ++j)
-    {
-      if (rawSpace.value().GetAccess({ i, j }) != Access::Accessable)
-      {
-        animation << "@";
-      }
-      else
-      {
-        animation << ".";
-      }
-    }
-
-    animation << "\n";
-  }
-
-  Time depth = 300;
-  std::shared_ptr<SpaceTime> space(new SpaceTime(depth, rawSpace.value()));
-  
-  // Read scenario
-  ScenarioLoader loader(TEST_DATA_PATH "/empty-16-16-even-1.scen");
-
-  int agentsNum = 51;
-  if (agentsNum > loader.GetNumExperiments())
-  {
-    std::cout << "too many agents, abort\n";
+    std::cout << "Animation init failed\n";
     return 1;
   }
+  std::cout << "Animation init ok\n";
 
-  for (int i = 0; i < agentsNum; ++i)
+
+  if (mission.ReadSpace(300, TEST_DATA_PATH "/empty-16-16.map"))
   {
-    Experiment agent = loader.GetNthExperiment(i);
-    space->SetAccess({ {agent.GetStartX(), agent.GetStartY()}, {0, depth} }, Access::Inaccessable);
+    std::cout << "ReadSpace failed\n";
+    return 1;
   }
+  std::cout << "ReadSpace ok\n";
 
-  for (int i = 0; i < agentsNum; ++i)
+  if (mission.InitAgents(51))
   {
-    // TODO add test when agent stands on one place
+    std::cout << "InitAgents failed\n";
+    return 1;
+  }
+  std::cout << "InitAgents ok\n";
 
-    std::cout << "Planning agent " << i << "... ";
 
-    // Prepare agent
-    Experiment agent = loader.GetNthExperiment(i);
-    Point start = { agent.GetStartX(), agent.GetStartY() };
-    Point goal = { agent.GetGoalX(), agent.GetGoalY() };
-
-    const SegmentHolder& segHolder = space->GetSegments(goal);
-    const auto iter = --segHolder.end(); // todo assert if any exists
-    Area origin = { start, {0, depth} };
-    Area destination = { goal, *iter };
-
-    space->SetAccess(origin, Access::Accessable);
-
-    // Prepare pathfinding
-    ArrayType<Move<Point>> moves =
-    {
-      Move<Point>{ 1, {0, 1}},
-      Move<Point>{ 1, {0, -1}},
-      Move<Point>{ 1, {1, 0}},
-      Move<Point>{ 1, {-1, 0}},
-    };
-
-    std::shared_ptr<MovesTestSegment> movesComponent(new MovesTestSegment(moves, space.get()));
-
-    std::shared_ptr<EuclideanHeuristic> hpoint(new EuclideanHeuristic(destination.point));
-    std::shared_ptr<Heuristic<Area>> h(new SpaceAdapter<Point, Area>(hpoint));
-
-    Pathfinder<Area> simplePathfinding(movesComponent, origin, h);
-
-    // Execute pathfinding
-    simplePathfinding.FindCost(destination);
-    if (!simplePathfinding.IsCostFound(destination))
-    {
-      std::cout << "failed to find agent with id = " << i << "\n";
-      break;
-    }
-
-    ArrayType<Node<Area>> path;
-    simplePathfinding.CollectPath(destination, path);
-    ArrayType<Area> inaccessableParts;
-    FromPathToFilledAreas(path, inaccessableParts);
-    space->MakeAreasInaccessable(inaccessableParts);
-
-    animation << "Agent " << i;
-    animation << " " << inaccessableParts[0].point.x << " " << inaccessableParts[0].point.y << " " << std::setprecision(4) << inaccessableParts[0].interval.start;
-    for (size_t i = 0; i + 1 < inaccessableParts.size(); i += 2)
-    {
-      Area currentArea = inaccessableParts[i];
-      Area nextArea = inaccessableParts[i + 1];
-      assert(nextArea.interval.IsValid());
-      animation << " " << currentArea.point.x << " " << currentArea.point.y << " " << std::setprecision(4) << nextArea.interval.start;
-      animation << " " << nextArea.point.x << " " << nextArea.point.y << " " << std::setprecision(4) << nextArea.interval.end;
-    }
-    animation << " " << inaccessableParts.back().point.x << " " << inaccessableParts.back().point.y << " " << std::setprecision(4) << inaccessableParts.back().interval.end;
-    animation << "\n";
-
-    std::cout << "success!\n";
+  if (mission.SolveCycle())
+  {
+    std::cout << "Planning failed\n";
+    return 1;
   }
 }
